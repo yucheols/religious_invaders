1ad3327020a2dc14ab382567d1259c75508ec176
Yucheol Shin <brongersmai2@gmail.com>
2025-04-01 18:08:26 -0400

    commit codes and files

--- codes/enm_G.R
+++ codes/enm_G.R
@@ -13,6 +13,7 @@ options(java.parameters = "-Xmx32G")
 # load packages
 library(terra)
 library(sf)
+library(SDMtune)
 library(ENMeval)
 library(ntbox)
 
@@ -25,28 +26,17 @@ glob.occs <- read.csv('data/occs/global/global_occs_thin_30km.csv')
 glob.occs$X <- NULL
 head(glob.occs)
 
-# load environment and resample to 10km // 5km*n = 10km ... agg factor of 2
+# load environmental variables
 glob.envs <- rast(list.files(path = 'data/envs/subset/global/', pattern = '.tif$', full.names = T))
-glob.envs <- aggregate(glob.envs, fact = 2)
 print(glob.envs)
 plot(glob.envs[[1]])
 
-# sample 20000 background points from the density raster
-#glob.kd <- biaslayer(occs_df = glob.occs, longitude = 'long', latitude = 'lat', raster_mold = raster::raster(glob.envs[[1]]))
-#plot(glob.kd)
-
-#glob.bg <- raster::xyFromCell(object = glob.kd, 
-#                              sample(which(!is.na(values(subset(x = glob.envs[[1]], 1)))), 
-#                                     size = 20000, prob = values(x = glob.kd)[!is.na(values(subset(x = glob.envs[[1]], 1)))])) %>% as.data.frame()
-#head(glob.bg)
-#write.csv(glob.bg, 'data/bg/global_20000_kd_10km.csv')
-
-glob.bg <- read.csv('data/bg/global_20000_kd_10km.csv')
+# load background data
+glob.bg <- read.csv('data/bg/global_20000_kd.csv')
 glob.bg$X <- NULL
 head(glob.bg)
 
 # plot points
-plot(glob.envs[[1]])
 points(glob.bg, col = 'red')
 points(glob.occs[, c('long', 'lat')], col = 'blue')
 
@@ -68,19 +58,23 @@ evalplot.grps(pts = glob.occs.narm, pts.grp = ckb2$occs.grp, envs = glob.envs)
 evalplot.grps(pts = glob.bg.narm, pts.grp = ckb2$bg.grp, envs = glob.envs)
 
 
-#####  part 3 ::: run models ----------
-glob.mod <- ENMevaluate(taxon.name = 'Mantis religiosa_Global',
-                        occs = glob.occs.narm,
-                        bg = glob.bg.narm,
-                        envs = glob.envs,
-                        tune.args = list(fc = c('L','LQ','H','LQH','LQHP','LQHPT'),
-                                         rm = seq(1,5, by = 0.5)),
-                        partitions = 'checkerboard',
-                        partition.settings = list(aggregation.factor = c(10,10)),
-                        algorithm = 'maxent.jar',
-                        doClamp = T)
+#####  part 3 ::: run models at 20km spatial resolution ----------
+# format data for SDMtune
+glob.swd <- prepareSWD(species = 'Mantis religiosa_Global', env = glob.envs, p = glob.occs.narm, a = glob.bg.narm, verbose = T)
+print(glob.swd)
 
-# save the output ENMevaluate object as RDS file
+# train a base model
+glob.mod <- SDMtune::train(method = 'Maxent', data = glob.swd, folds = ckb2, iter = 5000, progress = T)
+
+# tune model parameters
+glob.tune <- gridSearch(model = glob.mod,
+                        hypers = list(reg = seq(1,5, by = 0.5),
+                                      fc = c('l', 'lq', 'h', 'lqh', 'lqhp', 'lqhpt')),
+                        metric = 'tss',
+                        interactive = F,
+                        progress = T)
+
+# save the output model object as RDS file
 saveRDS()
 
 # select optimal model parameter combination
