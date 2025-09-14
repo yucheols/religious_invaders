587823504bc3e2f8255fae747408d47f4a14a53b
Yucheol Shin <brongersmai2@gmail.com>
2025-09-14 17:04:33 -0400

    commit files

--- codes/Shell/run_glob.sh
+++ codes/Shell/run_glob.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
-#SBATCH --job-name=religiosa_glob_30km
+#SBATCH --job-name=religiosa_glob_10km
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=30
+#SBATCH --time=144:00:00
+#SBATCH --mem=120G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yshin@amnh.org
+#SBATCH --output=/home/yshin/mendel-nas1/religiosa_nsdm_HPC/models_run/global_10km/output/log/religiosa_glob_10km_%j-%x.log
 
-
+# activate conda environment that contains R and all necessary packages
 conda activate nsdm_hpc
 cd /home/yshin/mendel-nas1/religiosa_nsdm_HPC  # this will be the R working directory on the cluster 
+
+# run the R script
+Rscript /home/yshin/mendel-nas1/religiosa_nsdm_HPC/scripts/R/enm_G_hpc.R
