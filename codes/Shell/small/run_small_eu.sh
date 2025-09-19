a3c7ab95e5bdd755cee165a1a28a9002c82f4eb2
Yucheol Shin <yshin@amnh.org>
2025-09-19 16:20:02 -0400

    commit

---
+++ codes/Shell/small/run_small_eu.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH --job-name=religiosa_eu_5km
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=30
+#SBATCH --time=144:00:00
+#SBATCH --mem=120G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yshin@amnh.org
+#SBATCH --output=/home/yshin/mendel-nas1/religiosa_nsdm_HPC/models_run/small/europe_5km/log/small_religiosa_eu_5km_%j-%x.log
+#SBATCH --error=/home/yshin/mendel-nas1/religiosa_nsdm_HPC/models_run/small/europe_5km/err/small_religiosa_eu_5km_%j-%x.err
+
+# load conda in batch mode
+source /home/yshin/mendel-nas1/miniconda3/etc/profile.d/conda.sh
+
+# activate conda environment that contains R and all necessary packages
+conda activate nsdm_hpc
+cd /home/yshin/mendel-nas1/religiosa_nsdm_HPC  # this will be the R working directory on the cluster 
+
+# run the R script
+Rscript /home/yshin/mendel-nas1/religiosa_nsdm_HPC/scripts/R/small/small_enm_N_hpc.R
